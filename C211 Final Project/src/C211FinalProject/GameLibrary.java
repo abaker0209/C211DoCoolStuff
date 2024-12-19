79aa4593ed808b4b698750d67ffdf8fb0f648561
DJSAMM <118455813+DJSAMM@users.noreply.github.com>
2024-12-19 04:18:40 -0500

    Update GameLibrary.java

--- C211 Final Project/src/C211FinalProject/GameLibrary.java
+++ C211 Final Project/src/C211FinalProject/GameLibrary.java
@@ -11,8 +11,28 @@ import java.util.Scanner;
 
 public class GameLibrary {
     
-    List library = new List();
-    
+    private List gameList; // Linked list for storing games
+    private Scanner scanner;
+
+    // Create game list and scanner
+    public GameLibrary() {
+        gameList = new List();
+        scanner = new Scanner(System.in);
+    }
+
+    // Case 1: Display all games in the list
+    public void displayGames() {
+        if (gameList.head == null) {
+            System.out.println("No games available.");
+        } else {
+            Node currentNode = gameList.head;
+            while (currentNode != null) {
+                System.out.println(currentNode.game.toString());
+                System.out.println("--------------------------");
+                currentNode = currentNode.next;
+            }
+        }
+    }
 
     // main method for testing
     public static void main(String[] args) {
@@ -39,32 +59,39 @@ public class GameLibrary {
             System.out.println("4. Return Game.");
             System.out.println("5. Add New Game to Library.");
             System.out.println("6. Remove Game from Library.");
-            System.out.println("6. Exit.");
+            System.out.println("7. Exit.");
 
             userChoice = scan.nextInt();
             switch (userChoice) { // need to add a try catch here
 
             case 1:
                 // display all games
+                displayGames()
                 break;
             case 2:
                 // search games
+                searchGames()
                 break;
             case 3:
                 // rent game
+                rentGame()
                 break;
             case 4:
                 // return game
+                returnGame()
                 break;
             case 5:
                 // Add new game to library
+                addGame()
                 break;
             case 6:
                 // remove game from library
+                removeGame()
                 break;
             case 7:
                 // exit 
-                run = false;
+                running = false;
+                System.out.println("Exiting program...");
                 break;
             default:
                 System.out.println("Invalid choice. Please try again.");
